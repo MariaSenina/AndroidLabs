0745514e815236d9eae97225adb5100766fdc83d
Maria <maria.senina15@gmail.com>
2022-01-26 19:23:47 -0500

    Add Listener to nextButtong: link to NameActivity

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextButton = findViewById(R.id.nextButton);
+        Intent nextPage = new Intent(this, NameActivity.class);
+        nextButton.setOnClickListener(click -> startActivity(nextPage));
     }
 
     @Override
