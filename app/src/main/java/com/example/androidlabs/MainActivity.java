94bea8e9c460a56d3cfd075d30ff30c034d8e306
Maria <maria.senina15@gmail.com>
2022-01-26 22:28:49 -0500

    Add SharedPreferences to MainActivity

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,20 +2,33 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences preferences;
+    EditText typeField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferences = getSharedPreferences("SavedPreferences", Context.MODE_PRIVATE);
+        String savedString = preferences.getString("ReserveName", "");
+        typeField = findViewById(R.id.inputText);
+        typeField.setText(savedString);
+
         Button nextButton = findViewById(R.id.nextButton);
         Intent nextPage = new Intent(this, NameActivity.class);
-        nextButton.setOnClickListener(click -> startActivity(nextPage));
+        nextButton.setOnClickListener(click -> {
+            startActivity(nextPage);
+
+        });
     }
 
     @Override
@@ -26,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        saveSharedPreferences(typeField.getText().toString());
     }
 
     @Override
@@ -41,5 +56,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    private void saveSharedPreferences(String stringToSave) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ReserveName", stringToSave);
+        editor.commit();
     }
 }
