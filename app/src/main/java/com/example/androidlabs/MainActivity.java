067f11347b4db1bb35dfdc615948f03e586aad3d
Maria <maria.senina15@gmail.com>
2022-02-09 11:30:56 -0500

    Add code to open db connection + Add TODO to db

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -19,11 +21,18 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<ToDoItem> items = new ArrayList();
+    private final static int TRUE = 1;
+    private final static int FALSE = 0;
+    SQLiteDatabase sqLiteDatabase;
+    ToDoItem newItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomOpener dbOpener = new CustomOpener(this);
+        sqLiteDatabase = dbOpener.getWritableDatabase();
 
         ListView listView = findViewById(R.id.todoListView);
 
@@ -34,8 +43,9 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(click -> {
             Switch urgentSwitch = findViewById(R.id.urgentSwitch);
             EditText todoEntry = findViewById(R.id.todoEntry);
-            ToDoItem newItem = new ToDoItem(todoEntry.getText().toString(), urgentSwitch.isChecked());
+            newItem = new ToDoItem(todoEntry.getText().toString(), urgentSwitch.isChecked());
             items.add(newItem);
+            addNewItemToDatabase();
             todoEntry.setText("");
             urgentSwitch.setChecked(false);
             adapter.notifyDataSetChanged();
@@ -59,6 +69,17 @@ public class MainActivity extends AppCompatActivity {
                     .create().show();
             return true;
         });
+    }
+
+    private void addNewItemToDatabase() {
+        ContentValues newRowValues = new ContentValues();
+        newRowValues.put(CustomOpener.COL_TODO_ITEM, newItem.getText());
+        if (newItem.isUrgent()) {
+            newRowValues.put(CustomOpener.COL_URGENT, TRUE);
+        } else {
+            newRowValues.put(CustomOpener.COL_URGENT, FALSE);
+        }
+        long id = sqLiteDatabase.insert(CustomOpener.TABLE_NAME, null, newRowValues);
     }
 
     private class CustomListAdapter extends BaseAdapter {
