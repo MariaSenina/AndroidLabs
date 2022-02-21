960f1a4cc4c5566cf7eca8c99ae8f2954113b278
Maria <maria.senina15@gmail.com>
2022-02-21 15:03:54 -0500

    Add CatImages class & Execute an instance of CatImages

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,7 +2,21 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CatImages request = new CatImages();
+        request.execute("https://cataas.com/cat?json=true");
+    }
+
+    class CatImages extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... args) {
+
+//            try {
+//                URL url = new URL(args[0]);
+//                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//                InputStream response = urlConnection.getInputStream();
+//
+//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+//                StringBuilder stringBuilder = new StringBuilder();
+//                String line = null;
+//                while ((line = bufferedReader.readLine()) != null) {
+//                    stringBuilder.append(line + "\n");
+//                }
+//
+//                String result = stringBuilder.toString();
+//
+//                JSONObject catImage = new JSONObject(result);
+//
+//                Bitmap currentPicture = BitmapFactory.decodeStream(response);
+//            } catch (IOException | JSONException e) {
+//                e.printStackTrace();
+//            }
+
+            return null;
+        }
     }
 }
