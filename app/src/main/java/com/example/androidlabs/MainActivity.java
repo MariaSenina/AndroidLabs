27dcf2f256c55d1ddcfdaf1bb06508211f9773de
Maria <maria.senina15@gmail.com>
2022-02-21 17:51:29 -0500

    Add display image from Bitmap functionality + Add cats_app banner

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,30 +16,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    private static final String DOMAIN = "https://cataas.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
 
-        CatImages request = new CatImages();
-        request.execute("https://cataas.com/cat?json=true");
+        CatImages request = new CatImages(MainActivity.this);
+        request.execute(DOMAIN + "/cat?json=true");
     }
 
     private class CatImages extends AsyncTask<String, Integer, String> {
+        private Context context;
+
+        public CatImages(Context context) {
+            this.context = context;
+        }
 
         @Override
         protected String doInBackground(String... args) {
-
+            InputStream response = null;
+            OutputStream output = null;
             try {
+                // Send first Web request
                 URL url = new URL(args[0]);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream response = urlConnection.getInputStream();
+                response = urlConnection.getInputStream();
+
+                // Read JSON
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
                 StringBuilder stringBuilder = new StringBuilder();
                 String line = null;
@@ -48,8 +63,16 @@ public class MainActivity extends AppCompatActivity {
                 String result = stringBuilder.toString();
 
                 JSONObject catImage = new JSONObject(result);
+                String imageUrl = catImage.getString("url");
+
+                // Send second request for cat image
+                url = new URL(DOMAIN + imageUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                response = urlConnection.getInputStream();
 
                 Bitmap currentPicture = BitmapFactory.decodeStream(response);
+
+                ((MainActivity)context).runOnUiThread(() -> imageView.setImageBitmap(currentPicture));
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
