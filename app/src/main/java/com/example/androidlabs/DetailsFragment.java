8f2c5443cef818abf17ca4cc94b7b63ace802b0f
Maria <maria.senina15@gmail.com>
2022-02-28 16:51:54 -0500

    Add layouts for phone and tablet

---
+++ app/src/main/java/com/example/androidlabs/DetailsFragment.java
@@ -0,0 +1,21 @@
+package com.example.androidlabs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+}
