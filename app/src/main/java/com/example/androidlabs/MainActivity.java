23a5056b8d0df70e687fcec224570a04dfa48a78
Maria <maria.senina15@gmail.com>
2022-03-06 20:10:56 -0500

    Setup new project with navbar support

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,119 +1,34 @@
 package com.example.androidlabs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.material.navigation.NavigationView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
-    public static final String NAME = "name";
-    public static final String HEIGHT = "height";
-    public static final String MASS = "mass";
-    public static final String BASE_URL = "https://swapi.dev/api/";
-
-    private List<SWCharacter> characters = new ArrayList<>();
-    private CustomListAdapter adapter;
-    private ListView listView;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView = findViewById(R.id.listView);
-
-        boolean isTablet = findViewById(R.id.fragment) != null;
-
-        StarWarsApiConsumer request = new StarWarsApiConsumer();
-        request.execute(BASE_URL + "people/?format=json");
-
-        listView.setOnItemClickListener((list, item, position, id) -> {
-            Bundle dataToSend = new Bundle();
-            dataToSend.putString(NAME, characters.get(position).getName());
-            dataToSend.putString(HEIGHT, characters.get(position).getHeight());
-            dataToSend.putString(MASS, characters.get(position).getMass());
-
-            if(isTablet) {
-                DetailsFragment detailFragment = new DetailsFragment();
-                detailFragment.setArguments(dataToSend);
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment, detailFragment)
-                        .commit();
-            } else {
-                Intent nextActivity = new Intent(MainActivity.this, EmptyActivity.class);
-                nextActivity.putExtras(dataToSend);
-                startActivity(nextActivity);
-            }
-        });
     }
 
-    private class StarWarsApiConsumer extends AsyncTask <String, Integer, Map<String, SWCharacter>> {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 
-        @Override
-        protected Map<String, SWCharacter> doInBackground(String... args) {
-            Map<String, SWCharacter> characterMap = new HashMap<>();
-            try {
-                InputStream response = makeHttpRequest(args[0]);
-                String result = parseJson(response);
-                JSONArray characters = new JSONObject(result).getJSONArray("results");
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
-                for (int i = 0; i < characters.length(); i++) {
-                    String name = characters.getJSONObject(i).getString(NAME);
-                    String height = characters.getJSONObject(i).getString(HEIGHT);
-                    String mass = characters.getJSONObject(i).getString(MASS);
-
-                    SWCharacter character = new SWCharacter(name, height, mass, i);
-                    characterMap.put(name, character);
-
-                    MainActivity.this.characters.add(character);
-                    publishProgress(i);
-                }
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-            }
-
-            return characterMap;
-        }
-
-        @Override
-        protected void onPostExecute(Map<String, SWCharacter> characterMap) {
-            adapter = new CustomListAdapter(MainActivity.this, characters);
-            listView.setAdapter(adapter);
-        }
-
-        protected InputStream makeHttpRequest(String address) throws IOException {
-            URL url = new URL(address);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            return urlConnection.getInputStream();
-        }
-
-        protected String parseJson(InputStream response) throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line + "\n");
-            }
-
-            return stringBuilder.toString();
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
